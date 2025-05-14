0a73e0a4ac8ce2f5f461a4c7562d4f25f7914e72
King-Kedyx <guillaume.gavignet@gmail.com>
2025-05-14 16:06:46 +0200

    Ajout du code pour le formulaire où il y a tous les contacts et mise dans le fichier main.js de tous les codes javascript

---
+++ fichiers_application/main.js
@@ -0,0 +1,52 @@
+
+function corriger() {
+    const reponses = {
+      q1: "b",
+      q2: "c",
+      q3: "c",
+      q4: "a",
+      q5: "b"
+    };
+
+    let erreurs = [];
+
+    for (let [q, bonneRep] of Object.entries(reponses)) {
+      const rep = document.querySelector(`input[name="${q}"]:checked`);
+      if (!rep || rep.value !== bonneRep) {
+        erreurs.push(q.slice(1));
+      }
+    }
+
+    const resultatDiv = document.getElementById("resultat");
+    const formulaireBtn = document.getElementById("formulaireBtn");
+
+    if (erreurs.length === 0) {
+      resultatDiv.textContent = "Bravo ! Toutes les réponses sont correctes.";
+      resultatDiv.classList.remove("text-red-600");
+      resultatDiv.classList.add("text-green-600");
+      formulaireBtn.classList.remove("hidden");
+    } else {
+      resultatDiv.textContent = "La réponse " + erreurs.join(", ") + " est incorrecte. Veuillez réessayer.";
+      resultatDiv.classList.remove("text-green-600");
+      resultatDiv.classList.add("text-red-600");
+      formulaireBtn.classList.add("hidden");
+    }
+}
+
+const display = document.getElementById("display");
+
+function appendToDisplay(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function calculate() {
+  try {
+    display.value = new Function('return ' + display.value)();
+  } catch (error) {
+    display.value = 'Erreur';
+  }
+}
